d2fb9894f6f13b7303c6fb245a3be43ed36c798f
grillc <80354835+grillc@users.noreply.github.com>
2021-03-29 21:22:15 +0200

    Ninesquare mit Dummykonstruktor und toString, ohne Methoden, noch ohne Interface

---
+++ src/main/java/Ninesquare.java
@@ -0,0 +1,40 @@
+
+public class Ninesquare {
+    private Tile[][] content;
+    private String name;
+    private boolean solved;
+
+
+    //dummy Konstruktor
+    Ninesquare() {
+        content = new Tile[9][9];
+        for (int row = 0; row <= 8; row++) {
+            for (int col = 0; col <= 8; col++) {
+                content[row][col] = new Tile( (col/3+1)+(row/3+1)*3,  (row+col)%9+1 );
+            }
+        }
+        name = "dummy";
+        solved = false;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Tile[] i : content) {
+            for (Tile j : i) {
+                sb.append(j).append(" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+
+
+
+
+
+}
