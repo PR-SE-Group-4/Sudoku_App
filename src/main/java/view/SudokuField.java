f74dc73008b9159fa15bc57caa1652dc197ac39d
Lisa Köberl <lisa.koeberl@outlook.com>
2021-05-08 16:32:03 +0200

    Konflikte in GUI anzeigen

--- src/main/java/view/SudokuField.java
+++ src/main/java/view/SudokuField.java
@@ -13,6 +13,7 @@ public class SudokuField extends JPanel {
     protected int selectedRow;
     protected int tileWidth = 50;
     protected int tileHeight = 50;
+    protected boolean showConflicts;
 
 
 
@@ -20,7 +21,44 @@ public class SudokuField extends JPanel {
         this.puzzle = puzzle;
         selectedCol = -1;
         selectedRow = -1;
+        showConflicts = false;
 
+    }
+
+    protected Color getTileColor(int square){
+        switch (square) {
+            case 0:
+                return Color.BLACK;
+            case 1:
+                return Color.GREEN;
+            case 2:
+                return Color.GRAY;
+            case 3:
+                return Color.blue;
+            case 4:
+                return Color.red;
+            case 5:
+                return Color.yellow;
+            case 6:
+                return Color.magenta;
+            case 7:
+                return Color.pink;
+            case 8:
+                return Color.CYAN;
+            case 9:
+                return Color.ORANGE;
+            default:
+                break;
+        }
+        return Color.WHITE;
+    }
+
+    public boolean isShowConflicts() {
+        return showConflicts;
+    }
+
+    public void setShowConflicts(boolean showConflicts) {
+        this.showConflicts = showConflicts;
     }
 
     public void inputActionListener(int x, int y, int value) {
