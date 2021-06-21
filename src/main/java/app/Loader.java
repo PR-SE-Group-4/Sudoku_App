2ed8227857d5eb4062b363f6db559267c6ec7c45
Lisa Köberl <lisa.koeberl@outlook.com>
2021-06-21 20:02:16 +0200

    Unittests für Klasse Loader erstellt

--- src/main/java/app/Loader.java
+++ src/main/java/app/Loader.java
@@ -21,22 +21,22 @@ public class Loader {
         if (!appdir.exists()) {
             appdir.mkdir();
             try {
-                File presaved00 = new File(appdir + "\\supereasy.txt");
+                File presaved00 = new File(appdir + "\\01supereasy.txt");
                 presaved00.createNewFile();
                 FileWriter presaved00wr = new FileWriter(presaved00);
                 presaved00wr.write("EASY;CLASSIC;false;000000;1U0-1U0-1O3-2O4-2O5-2O6-3O7-3U0-3O9-1O4-1O5-1O6-2O7-2O8-2U0-3O1-3O2-3O3-1O7-1O8-1O9-2U0-2O2-2O3-3O4-3O5-3O6-4O2-4U0-4O4-5O5-5O6-5O7-6O8-6O9-6U0-4O5-4O6-4O7-5O8-5O9-5O1-6U0-6O3-6O4-4O8-4O9-4O1-5O2-5U0-5O4-6O5-6O6-6O7-7O3-7O4-7U0-8O6-8O7-8O8-9O9-9O1-9O2-7U0-7O7-7O8-8O9-8O1-8O2-9O3-9U0-9O5-7O9-7O1-7O2-8O3-8O4-8U0-9O6-9O7-9O8-;");
                 presaved00wr.close();
-                File presaved01 = new File(appdir + "\\medium01.txt");
+                File presaved01 = new File(appdir + "\\01medium.txt");
                 presaved01.createNewFile();
                 FileWriter presaved01wr = new FileWriter(presaved01);
                 presaved01wr.write("NORMAL;CLASSIC;false;000000;1U0-1U0-1O4-2U0-2O8-2O6-3U0-3O1-3U0-1U0-1O1-1U0-2U0-2O3-2U0-3U0-3U0-3O4-1U0-1U0-1O3-2O2-2U0-2O4-3O6-3O9-3U0-4O1-4U0-4O5-5O6-5O7-5U0-6U0-6U0-6O3-4O8-4U0-4U0-5U0-5U0-5U0-6U0-6O5-6U0-4U0-4U0-4O2-5U0-5O5-5U0-6O4-6U0-6O9-7U0-7U0-7U0-8U0-8U0-8U0-9O9-9U0-9U0-7O3-7U0-7O6-8U0-8O9-8O1-9O7-9U0-9U0-7U0-7O8-7U0-8U0-8U0-8U0-9U0-9U0-9U0-;");
                 presaved01wr.close();
-                File presaved02 = new File(appdir + "\\samurai01.txt");
+                File presaved02 = new File(appdir + "\\01samurai.txt");
                 presaved02.createNewFile();
                 FileWriter presaved02wr = new FileWriter(presaved02);
                 presaved02wr.write("HARD;SAMURAI;false;000000;1O2-1U0-1O5-2U0-2U0-2U0-3O9-3U0-3O4-1U0-1O7-1U0-2U0-2U0-2U0-3U0-3O5-3U0-1O8-1U0-1U0-2U0-2O6-2U0-3U0-3U0-3O1-4U0-4U0-4U0-5O4-5U0-5O9-6U0-6U0-6U0-4U0-4U0-4O6-5U0-5U0-5U0-6O2-6U0-6U0-4U0-4U0-4U0-5O6-5U0-5O2-6U0-6U0-6U0-7O4-7U0-7U0-8U0-8O8-8U0-9U0-9U0-9O9-7U0-7O3-7U0-8U0-8U0-8U0-9U0-9O1-9U0-7O1-7U0-7O8-8U0-8U0-8U0-9O4-9U0-9O7-x1O6-1U0-1O7-2U0-2U0-2U0-3O3-3U0-3O1-1U0-1O9-1U0-2U0-2U0-2U0-3U0-3O7-3U0-1O1-1U0-1U0-2U0-2O2-2U0-3U0-3U0-3O9-4U0-4U0-4U0-5O1-5U0-5O4-6U0-6U0-6U0-4U0-4U0-4O3-5U0-5U0-5U0-6O5-6U0-6U0-4U0-4U0-4U0-5O2-5U0-5O8-6U0-6U0-6U0-7O5-7U0-7U0-8U0-8O6-8U0-9U0-9U0-9O4-7U0-7O4-7U0-8U0-8U0-8U0-9U0-9O8-9U0-7O8-7U0-7O6-8U0-8U0-8U0-9O1-9U0-9O5-x1U0-1U0-1O9-2U0-2U0-2U0-3O5-3U0-3U0-1U0-1O1-1U0-2U0-2U0-2U0-3U0-3O4-3U0-1O4-1U0-1O7-2U0-2U0-2U0-3O8-3U0-3O6-4U0-4U0-4U0-5O4-5U0-5O3-6U0-6U0-6U0-4U0-4U0-4U0-5U0-5O7-5U0-6U0-6U0-6U0-4U0-4U0-4U0-5O2-5U0-5O9-6U0-6U0-6U0-7O1-7U0-7O4-8U0-8U0-8U0-9O2-9U0-9O5-7U0-7O9-7U0-8U0-8U0-8U0-9U0-9O1-9U0-7U0-7U0-7O8-8U0-8U0-8U0-9O4-9U0-9U0-x1O8-1U0-1O9-2U0-2U0-2U0-3O1-3U0-3O4-1U0-1O4-1U0-2U0-2U0-2U0-3U0-3O9-3U0-1O2-1U0-1U0-2U0-2O7-2U0-3U0-3U0-3O8-4U0-4U0-4U0-5O7-5U0-5O8-6U0-6U0-6U0-4U0-4U0-4O2-5U0-5U0-5U0-6O6-6U0-6U0-4U0-4U0-4U0-5O1-5U0-5O2-6U0-6U0-6U0-7O9-7U0-7U0-8U0-8O2-8U0-9U0-9U0-9O5-7U0-7O2-7U0-8U0-8U0-8U0-9U0-9O4-9U0-7O1-7U0-7O4-8U0-8U0-8U0-9O9-9U0-9O7-x1O2-1U0-1O5-2U0-2U0-2U0-3O3-3U0-3O7-1U0-1O1-1U0-2U0-2U0-2U0-3U0-3O6-3U0-1O4-1U0-1U0-2U0-2O1-2U0-3U0-3U0-3O8-4U0-4U0-4U0-5O2-5U0-5O9-6U0-6U0-6U0-4U0-4U0-4O8-5U0-5U0-5U0-6O6-6U0-6U0-4U0-4U0-4U0-5O1-5U0-5O4-6U0-6U0-6U0-7O7-7U0-7U0-8U0-8O9-8U0-9U0-9U0-9O6-7U0-7O3-7U0-8U0-8U0-8U0-9U0-9O9-9U0-7O6-7U0-7O4-8U0-8U0-8U0-9O5-9U0-9O3-;");
                 presaved02wr.close();
-                File presaved03 = new File(appdir + "\\freeform01.txt");
+                File presaved03 = new File(appdir + "\\01freeform.txt");
                 presaved03.createNewFile();
                 FileWriter presaved03wr = new FileWriter(presaved03);
                 presaved03wr.write("HARD;FREEFORM;false;000000;1O5-2U0-2O3-2U0-3U0-3O1-3O7-3U0-3U0-1U0-1U0-2U0-2U0-2U0-3U0-3U0-3U0-4O3-1O8-1U0-1O4-2O6-2U0-2O7-3U0-4U0-4U0-1O1-1U0-8U0-9U0-9O6-9U0-4U0-4U0-4U0-1U0-8U0-8U0-9O3-9U0-9O9-4U0-4U0-5U0-8U0-8U0-8U0-9U0-9O8-9U0-4U0-5U0-5O9-8U0-8U0-7U0-6O5-6U0-6O3-5O4-5U0-5O1-8O3-7U0-7U0-7U0-6U0-6U0-6U0-5U0-5U0-7U0-7U0-7O5-7O8-7U0-6U0-6O1-6U0-5O7-;");
@@ -100,7 +100,7 @@ public class Loader {
         FileReader fileReader = new FileReader(files[nrOfFile]);
 
         String name = files[nrOfFile].toString();
-        name = name.substring(name.indexOf("4")+1, name.indexOf("."));
+        name = name.substring(name.indexOf("4")+2, name.indexOf("."));
         Difficulty difficulty = null;
         Type type = null;
         boolean solved = true;
---
+++ src/test/java/LoaderTest.java
@@ -0,0 +1,111 @@
+import app.Loader;
+import model.Difficulty;
+import model.Ninesquare;
+import model.Puzzle;
+import model.Type;
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class LoaderTest {
+    File folder;
+    File presaved00;
+    File presaved01;
+    File presaved02;
+    File presaved03;
+    File [] files;
+    ArrayList<Puzzle> storedPuzzles;
+    Puzzle freeform;
+    Puzzle samurai;
+    Puzzle newPuzzle;
+    Puzzle newTemplate;
+    File testPuzzle;
+    File testTemplate;
+
+    @BeforeAll
+    void init() {
+        folder = new File(System.getenv("APPDATA")+"\\SudokuGR04");
+        presaved00 = new File(folder + "\\01freeform.txt");
+        presaved01 = new File(folder + "\\01medium.txt");
+        presaved02 = new File(folder + "\\01samurai.txt");
+        presaved03 = new File(folder + "\\01supereasy.txt");
+        storedPuzzles = new ArrayList<>();
+        newPuzzle = new Ninesquare("Test", Difficulty.NORMAL, Type.FREEFORM, false, 0, "1O5-2U0-2O3-2U0-3U0-3O1-3O7-3U0-3U0-1U0-1U0-2U0-2U0-2U0-3U0-3U0-3U0-4O3-1O8-1U0-1O4-2O6-2U0-2O7-3U0-4U0-4U0-1O1-1U0-8U0-9U0-9O6-9U0-4U0-4U0-4U0-1U0-8U0-8U0-9O3-9U0-9O9-4U0-4U0-5U0-8U0-8U0-8U0-9U0-9O8-9U0-4U0-5U0-5O9-8U0-8U0-7U0-6O5-6U0-6O3-5O4-5U0-5O1-8O3-7U0-7U0-7U0-6U0-6U0-6U0-5U0-5U0-7U0-7U0-7O5-7O8-7U0-6U0-6O1-6U0-5O7-");
+        newTemplate = new Ninesquare();
+        //newTemplate.setEntry(99,0,0,5);
+        //newTemplate.setEntry(99, 3,3,6);
+        //newTemplate.setEntry(99, 7,7,9);
+        testPuzzle = new File(folder + "\\testPuzzle.txt");
+        testTemplate = new File(folder + "\\testTemplate.txt");
+    }
+
+    @Test
+    @Order(1)
+    void setup() {
+        Loader.setup();
+
+        assertTrue(folder.exists());
+        assertTrue(folder.isDirectory());
+        assertTrue(presaved00.exists());
+        assertTrue(presaved01.exists());
+        assertTrue(presaved02.exists());
+        assertTrue(presaved03.exists());
+    }
+
+    @Test
+    @Order(2)
+    void getFiles() {
+        files = Loader.getFiles();
+        assertEquals(files[0], presaved00);
+        assertEquals(files[1], presaved01);
+        assertEquals(files[2], presaved02);
+        assertEquals(files[3], presaved03);
+    }
+
+    @Test
+    @Order(3)
+    void getStoredPuzzles() {
+        storedPuzzles = Loader.getStoredPuzzles();
+        assertFalse(storedPuzzles.isEmpty());
+        assertEquals(storedPuzzles.get(0).getName(), "01freeform");
+        assertEquals(storedPuzzles.get(1).getName(), "01medium");
+        assertEquals(storedPuzzles.get(2).getName(), "01samurai");
+        assertEquals(storedPuzzles.get(3).getName(), "01supereasy");
+    }
+
+    @Test
+    @Order(4)
+    void loadPuzzle() throws Exception {
+        freeform = Loader.loadPuzzle(0);
+        samurai = Loader.loadPuzzle(2);
+
+        assertNotNull(freeform);
+        assertNotNull(samurai);
+        assertEquals("01freeform", freeform.getName());
+        assertEquals(Type.FREEFORM, freeform.getType());
+        assertEquals("01samurai", samurai.getName());
+        assertEquals(Type.SAMURAI, samurai.getType());
+
+    }
+
+    @Test
+    @Order(5)
+    void saveGame() {
+        Loader.saveGame(newPuzzle, "testPuzzle");
+        assertTrue(testPuzzle.exists());
+    }
+
+    @Test
+    @Order(6)
+    void saveTemplate() {
+        Loader.saveTemplate(newTemplate, "testTemplate");;
+        assertTrue(testTemplate.exists());
+        /*assertEquals(5, newTemplate.getTile(99,0,0).getEntry());
+        assertFalse(newTemplate.getTile(99, 0,0).isChangeable());*/
+    }
+}
