c20653c28dc578f7d37abc8c7d3d1e9de9442f9e
Lisa Köberl <lisa.koeberl@outlook.com>
2021-06-28 11:46:52 +0200

    LoaderTest angepasst

--- src/test/java/LoaderTest.java
+++ src/test/java/LoaderTest.java
@@ -6,6 +6,7 @@ import model.Type;
 import org.junit.jupiter.api.*;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -37,9 +38,9 @@ class LoaderTest {
         storedPuzzles = new ArrayList<>();
         newPuzzle = new Ninesquare("Test", Difficulty.NORMAL, Type.FREEFORM, false, 0, "1O5-2U0-2O3-2U0-3U0-3O1-3O7-3U0-3U0-1U0-1U0-2U0-2U0-2U0-3U0-3U0-3U0-4O3-1O8-1U0-1O4-2O6-2U0-2O7-3U0-4U0-4U0-1O1-1U0-8U0-9U0-9O6-9U0-4U0-4U0-4U0-1U0-8U0-8U0-9O3-9U0-9O9-4U0-4U0-5U0-8U0-8U0-8U0-9U0-9O8-9U0-4U0-5U0-5O9-8U0-8U0-7U0-6O5-6U0-6O3-5O4-5U0-5O1-8O3-7U0-7U0-7U0-6U0-6U0-6U0-5U0-5U0-7U0-7U0-7O5-7O8-7U0-6U0-6O1-6U0-5O7-");
         newTemplate = new Ninesquare();
-        //newTemplate.setEntry(99,0,0,5);
-        //newTemplate.setEntry(99, 3,3,6);
-        //newTemplate.setEntry(99, 7,7,9);
+        newTemplate.setEntry(99,0,0,5);
+        newTemplate.setEntry(99, 3,3,6);
+        newTemplate.setEntry(99, 7,7,9);
         testPuzzle = new File(folder + "\\testPuzzle.txt");
         testTemplate = new File(folder + "\\testTemplate.txt");
     }
@@ -93,19 +94,23 @@ class LoaderTest {
 
     }
 
-  /*  @Test
+    @Test
     @Order(5)
-    void saveGame() {
-        Loader.saveGame(newPuzzle, "testPuzzle");
+    void saveGame() throws IOException {
+        try {
+            Loader.saveGame(newPuzzle, "testPuzzle");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         assertTrue(testPuzzle.exists());
     }
-*/
+
     @Test
     @Order(6)
     void saveTemplate() {
         Loader.saveTemplate(newTemplate, "testTemplate");;
         assertTrue(testTemplate.exists());
-        /*assertEquals(5, newTemplate.getTile(99,0,0).getEntry());
-        assertFalse(newTemplate.getTile(99, 0,0).isChangeable());*/
+        assertEquals(5, newTemplate.getTile(99,0,0).getEntry());
+        assertFalse(newTemplate.getTile(99, 0,0).isChangeable());
     }
 }
